8a4b0d0535125fe70633060d0326204fe3e925b1
Juliano Hiroi <julianohiroi@gmail.com>
2024-07-09 18:34:01 -0300

    first commit

---
+++ src/main/java/com/gof/GofSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.gof;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GofSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GofSpringBootApplication.class, args);
+	}
+
+}
